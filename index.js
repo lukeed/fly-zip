54131b7b4561ca8426476b1d011e83cdd56a0d8b
luke <luke.edwards05@gmail.com>
2016-04-03 12:43:26 -0700

    temp placeholder

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-module.exports = function (/*debug*/) {
-  this.filter("zip", (data, options) => {
-  /**
-    @overview A filter plugin returns an object { code, map, ext }
-    which is the result of transforming the incomding data source:
+var yazl = require('yazl');
 
-      return { code, map, ext }
+module.exports = function () {
+	var zip = new Yazl.ZipFile();
 
-    @example Sync filter `j` that transforms a given string into an
-    object, i.e, {code, map} where code is the result data and map
-    a sourcemap if `options.sourceMap === true`.
+	this.filter('zip', function (data, opts) {
+		opts = opts || {};
+		opts.compress = typeof opts.compress === 'boolean' ? opts.compress : true;
 
-      const j = require("my-js-transformer")
-      const assign = require("object-assign")
-
-      module.exports = function () {
-        return this.filter("j", (data, options) => {
-          return assign({ ext: ".js"}, j.render(data.toString(), options))
-        })
-      }
-
-    @example Async filter `s` that transforms a given string and invokes
-    a callback function with an object, i.e, {css, map}.
-
-    const s = require("my-style-trasformer")
-    const assign = require("object-assign")
-
-    module.exports = function () {
-      return this.defer(s.render)(data.toString(), options).then((result) =>
-        assign({ ext: ".css"}, result))
-    }
-  */
-  })
-}
+		// zip.addBuffer(data, )
+	}
+};
