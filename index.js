00cf7d0fec5986c63e2e42a72bab6a5bcf155b6e
luke <luke.edwards05@gmail.com>
2016-04-04 11:24:04 -0700

    plugin logic, functional!

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
+var path = require('path');
 var yazl = require('yazl');
+var stat = require('fs').statSync;
+var write = require('fs').writeFile;
+var assign = require('object-assign');
+var concat = require('concat-stream');
 
 module.exports = function () {
-	var zip = new Yazl.ZipFile();
+	var opts;
+	var self = this;
+	var zip = new yazl.ZipFile();
 
-	this.filter('zip', function (data, opts) {
-		opts = opts || {};
-		opts.compress = typeof opts.compress === 'boolean' ? opts.compress : true;
+	self.zip = function (options) {
+		opts = opts || assign({
+			base: '',
+			compress: true,
+			destination: this.root,
+			filename: 'archive.zip'
+		}, options || {});
 
-		// zip.addBuffer(data, )
+		return self.unwrap(function (files) {
+			files.forEach(handler);
+		}).then(function () {
+			zip.end(function () {
+				zip.outputStream.pipe(concat(function (data) {
+					return write(path.join(opts.destination, opts.filename), data, catcher);
+				}));
+			});
+		});
+	};
+
+	function handler(realPath) {
+		var metaPath = path.relative(opts.base, realPath);
+		var stats = stat(realPath);
+
+		if (stats.isDirectory()) {
+			if (stats.nlink < 3) {
+				zip.addEmptyDirectory(metaPath);
+			}
+		} else {
+			zip.addFile(realPath, metaPath, opts);
+		}
+	}
+
+	function catcher(err) {
+		if (err) {
+			self.emit('plugin_error', {
+				plugin: 'fly-zip',
+				error: err.message
+			});
+		}
 	}
 };
