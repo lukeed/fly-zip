08c777d8466cc6f729f4e53aec6189c42849045e
Luke Edwards <luke.edwards05@gmail.com>
2016-10-20 23:05:13 +0200

    upgrade plugin logic

--- index.js
+++ index.js
@@ -1,53 +1,25 @@
-var path = require('path');
-var yazl = require('yazl');
-var stat = require('fs').statSync;
-var assign = require('object-assign');
-var concat = require('concat-stream');
-var writer = require('safe-write-file');
+'use strict';
+
+const p = require('path');
+const JSZip = require('jszip');
 
 module.exports = function () {
-	var opts;
-	var self = this;
-	var zip = new yazl.ZipFile();
+	const zip = new JSZip();
 
-	self.zip = function (options) {
-		opts = opts || assign({
-			base: '',
-			compress: true,
-			destination: '.',
-			filename: 'archive.zip'
-		}, options || {});
+	this.plugin('zip', {every: 0}, function * (files, opts) {
+		opts = Object.assign({
+			dest: '.',
+			name: 'archive.zip'
+		}, opts);
 
-		return self.unwrap(function (files) {
-			files.forEach(handler);
-		}).then(function () {
-			zip.end(function () {
-				zip.outputStream.pipe(concat(function (data) {
-					return writer(path.join(self.root, opts.destination, opts.filename), data, catcher);
-				}));
-			});
-		});
-	};
-
-	function handler(realPath) {
-		var metaPath = path.relative(opts.base, realPath);
-		var stats = stat(realPath);
-
-		if (stats.isDirectory()) {
-			if (stats.nlink < 3) {
-				zip.addEmptyDirectory(metaPath);
+		for (const file of files) {
+			if (file.data) {
+				zip.file(p.relative(this.root, p.format(file)), file.data, {base64: 1});
 			}
-		} else {
-			zip.addFile(realPath, metaPath, opts);
 		}
-	}
 
-	function catcher(err) {
-		if (err) {
-			self.emit('plugin_error', {
-				plugin: 'fly-zip',
-				error: err.message
-			});
-		}
-	}
+		const out = yield zip.generateAsync({type: 'nodebuffer'});
+
+		yield this.$.write(p.resolve(opts.dest, opts.name), out);
+	});
 };
